8abaa90e9ba2fa538e644c9b394a4550b4346801
michalmlynarczyk <mlymichal@gmail.com>
2026-07-10 20:43:28 +0300

    Configure domain routing for korepetycje

--- backend/config/settings/dev.py
+++ backend/config/settings/dev.py
@@ -2,4 +2,10 @@ from .base import *
 
 
 DEBUG = True
-ALLOWED_HOSTS = ALLOWED_HOSTS or ['localhost', '127.0.0.1', '45.93.139.211']
+ALLOWED_HOSTS = ALLOWED_HOSTS or [
+    'localhost',
+    '127.0.0.1',
+    '45.93.139.211',
+    'nastomatma.pl',
+    'www.nastomatma.pl',
+]
